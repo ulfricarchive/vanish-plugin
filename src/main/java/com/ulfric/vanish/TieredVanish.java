e04a68089f78b72f1a5dcc7a75e11340d4a1fb07
Adam Harrison <ad@mharrison.us>
2017-09-01 20:04:43 -0400

    Persistent vanish

--- src/main/java/com/ulfric/vanish/TieredVanish.java
+++ src/main/java/com/ulfric/vanish/TieredVanish.java
@@ -3,17 +3,42 @@ package com.ulfric.vanish;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import com.ulfric.data.database.Data;
+import com.ulfric.data.database.Database;
+import com.ulfric.data.database.Store;
 import com.ulfric.embargo.entity.User;
 import com.ulfric.embargo.limit.Limit;
+import com.ulfric.servix.ServiceApplication;
 import com.ulfric.servix.services.vanish.VanishService;
 
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-public class TieredVanish implements VanishService { // TODO thread safety, should we take players out of vanish when the container is disabled?
+public class TieredVanish extends ServiceApplication implements VanishService { // TODO thread safety, should we take players out of vanish when the container is disabled?
 
 	private final Set<UUID> vanished = new HashSet<>(); // TODO make vanish persistent
+
+	@Database
+	private Store users;
+
+	public TieredVanish() {
+		addBootHook(this::loadUserData);
+	}
+
+	private void loadUserData() {
+		users.getAllData()
+			.forEach(this::loadUserData);
+	}
+
+	private void loadUserData(Data data) {
+		boolean vanished = data.getBoolean("vanished");
+
+		if (vanished) {
+			UUID uniqueId = data.getUniqueId("uniqueId");
+			this.vanished.add(uniqueId);
+		}
+	}
 
 	@Override
 	public Class<VanishService> getService() {
@@ -22,7 +47,10 @@ public class TieredVanish implements VanishService { // TODO thread safety, shou
 
 	@Override
 	public void vanish(UUID uniqueId) {
-		vanished.add(uniqueId);
+		if (vanished.add(uniqueId)) {
+			writeData(uniqueId, true);
+		}
+
 		Player vanisher = Bukkit.getPlayer(uniqueId);
 		User vanishingUser = User.getUser(uniqueId);
 		Limit vanishLevel = vanishingUser.getLimit("vanish");
@@ -49,6 +77,7 @@ public class TieredVanish implements VanishService { // TODO thread safety, shou
 	@Override
 	public void unvanish(UUID uniqueId) {
 		if (vanished.remove(uniqueId)) {
+			writeData(uniqueId, false);
 			Player vanisher = Bukkit.getPlayer(uniqueId);
 
 			if (vanisher == null) {
@@ -60,9 +89,15 @@ public class TieredVanish implements VanishService { // TODO thread safety, shou
 					continue;
 				}
 
-				online.showPlayer(vanisher); // TODO handle vanishing from other plugins
+				online.showPlayer(vanisher);
 			}
 		}
+	}
+
+	private void writeData(UUID uniqueId, boolean vanished) {
+		Data data = users.getData(uniqueId);
+		data.set("uniqueId", uniqueId);
+		data.setBoolean("vanished", true);
 	}
 
 	@Override
