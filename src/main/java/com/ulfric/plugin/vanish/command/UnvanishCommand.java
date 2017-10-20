6f16e9de8a4b13359e9358d220c41e18349c14b4
Adam Harrison <ad@mharrison.us>
2017-10-20 04:09:37 +0300

    Implement /vanish and /unvanish

--- src/main/java/com/ulfric/plugin/vanish/command/UnvanishCommand.java
+++ src/main/java/com/ulfric/plugin/vanish/command/UnvanishCommand.java
@@ -1,19 +1,31 @@
 package com.ulfric.plugin.vanish.command;
 
+import java.util.UUID;
+
 import com.ulfric.commons.naming.Name;
 import com.ulfric.plugin.commands.Alias;
 import com.ulfric.plugin.commands.Command;
-import com.ulfric.plugin.commands.Context;
 import com.ulfric.plugin.commands.Permission;
+import com.ulfric.plugin.vanish.VanishService;
 
 @Name("unvanish")
 @Alias("unv")
 @Permission("vanish.unvanish")
-public class UnvanishCommand implements Command {
+public class UnvanishCommand extends Command {
 
 	@Override
-	public void run(Context context) {
-		// TODO
+	public void run() {
+		VanishService vanish = VanishService.get();
+		UUID uniqueId = uniqueId();
+
+		if (!vanish.isVanished(uniqueId)) {
+			tell("unvanish-not-vanished");
+			return;
+		}
+
+		vanish.unvanish(uniqueId());
+
+		tell(vanish.isVanished(uniqueId) ? "unvanish-failed" : "unvanish");
 	}
 
 }
--- src/main/java/com/ulfric/plugin/vanish/command/VanishCommand.java
+++ src/main/java/com/ulfric/plugin/vanish/command/VanishCommand.java
@@ -1,19 +1,31 @@
 package com.ulfric.plugin.vanish.command;
 
+import java.util.UUID;
+
 import com.ulfric.commons.naming.Name;
 import com.ulfric.plugin.commands.Alias;
 import com.ulfric.plugin.commands.Command;
-import com.ulfric.plugin.commands.Context;
 import com.ulfric.plugin.commands.Permission;
+import com.ulfric.plugin.vanish.VanishService;
 
 @Name("vanish")
 @Alias("v")
 @Permission("vanish.vanish")
-public class VanishCommand implements Command {
+public class VanishCommand extends Command {
 
 	@Override
-	public void run(Context context) {
-		// TODO
+	public void run() {
+		VanishService vanish = VanishService.get();
+		UUID uniqueId = uniqueId();
+
+		if (vanish.isVanished(uniqueId)) {
+			tell("vanish-already-vanished");
+			return;
+		}
+
+		vanish.vanish(uniqueId());
+
+		tell(vanish.isVanished(uniqueId) ? "vanish" : "vanish-failed");
 	}
 
 }
